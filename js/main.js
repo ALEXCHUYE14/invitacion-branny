27d50297621b43b8d31184f0295b51008ddb1502
Miguel Alexander Flores Chuye <floreschuyem@gmail.com>
2026-06-18 23:22:25 -0500

    Invitacion

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var CFG = {
   whatsapp:  "51950729460",
   eventDate: "2026-07-10T20:00:00-05:00",
   mapsQuery: "Gaetano Sullana Piura Perú",
-  sheetsUrl: ""
+  sheetsUrl: "https://script.google.com/macros/s/AKfycbw0zONZALbHB7Ax0Z6MRsE2teIXTR3ityE33m9XDmZS5rQgP7U2-5xOlwv5t0YtrnncZQ/exec"
 };
 
 /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
